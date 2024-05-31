33d588c05428313f9180a70e45b0dfbb613d75a9
Art Frich <frich.g23@yandex.ru>
2024-05-31 19:17:55 +0500

    feat/добавить вторую кнопку

--- ch7_4/src/pages/Private2.jsx
+++ ch7_4/src/pages/Private2.jsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
-import LogoutButton from '../Components/LogoutButton.jsx';
 import axios from 'axios'
+import LogoutButton from '../Components/LogoutButton'
 import { create } from '@github/webauthn-json'
 
 const Private2 = () => {
   const [busy, setBusy] = useState(false)
   const [message, setMessage] = useState()
 
+  const registerToken = async (startRegistrationEndpoint) => {
+    setBusy(true)
+    try {
+      const response = await axios.post(startRegistrationEndpoint)
+      setMessage('Send response')
+      const attestation = await create({ publicKey: response.data })
+      setMessage('Create attestation')
+      const attestationResponse = await axios.post('/register', {
+        attestation,
+      })
+      setMessage('registered!')
+      if (
+        attestationResponse.data &&
+        attestationResponse.data.verified
+      ) {
+        alert('New key registered')
+      }
+    } catch (err) {
+      setMessage('' + err)
+    } finally {
+      setBusy(false)
+    }
+  }
   return (
     <div className="Private2">
       <h1>Account page</h1>
@@ -15,35 +38,16 @@ const Private2 = () => {
         <>
           <p>Register new hardware key</p>
           <button
-            onClick={async () => {
-              setBusy(true)
-              try {
-                const response = await axios.post('/startRegister')
-                setMessage('Send response')
-
-                const attestation = await create({
-                  publicKey: response.data,
-                })
-                setMessage('Create attestation')
-
-                const attestationResponse = await axios.post('/register', 
-                  { attestation,}
-                )
-                setMessage('registered!')
-
-                if ( attestationResponse.data?.verified) {
-                  alert('New key registered')
-                }
-
-              } catch (err) {
-                setMessage('' + err)
-              } finally {
-                setBusy(false)
-              }
-            }}
+            onClick={() => registerToken('/startRegister')}
             disabled={busy}
           >
-            Register
+            Register Removable Token
+          </button>
+          <button
+            onClick={() => registerToken('/startFingerprint')}
+            disabled={busy}
+          >
+            Register Fingerprint
           </button>
         </>
       )}
